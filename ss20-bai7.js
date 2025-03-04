6b1e5d47b7274227fc24949fd3039cc719dbae5b
ngoc-dan <ngocdan191026@gmail.com>
2025-03-04 16:08:54 +0700

    Add files via upload

--- ss20-bai7.js
+++ ss20-bai7.js
@@ -1,12 +1,16 @@
 let n = +prompt("nhap n so con in ra")
-let str = "1" + " " + "1"
+let str = "1"
 let sum = 0
-let num1=1
-let num2=1
-for(let i=2; i< n;i++ ){
+let num1 = 1
+let num2 = 0
+for (let i = 1; i < n; i++) {
     sum = num1 + num2
     str = str + " " + sum
-    num2=num1
-    num1=sum
+    num2 = num1
+    num1 = sum
 }
-alert(str)
+if (n>0){
+    alert(str)
+}else{
+    alert("du lieu khong hop le")
+}
